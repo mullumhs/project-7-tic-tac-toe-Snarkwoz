48e84b0db7e1cb8b7ebe99303cae8e95e80d5a48
Snarkwoz <140124176+Snarkwoz@users.noreply.github.com>
2024-04-10 10:40:07 +1000

    fneofnose

--- connect-four.py
+++ connect-four.py
@@ -4,10 +4,30 @@ def initialise_board():
     for i in range(6):
         board.append(["-", "-", "-", "-", "-", "-", "-"])
 
+def player_turn():
+    if currentplayer == "x":
+        currentplayer = "o"
+    elif currentplayer == "o":
+        currentplayer = "x"
+    print(f"Test: {currentplayer}")
+
+
 def userinput():
-    col = int(input("Select col: "))
+    col = input("Select column (1 - 6): ")
+    while True:
+        while not col.isdigit():
+            print("Must be a number between 1 and 6")
+            col = input("Select column (1 - 6): ")
+            while not col in range(1,7):
+                print("Must be a number between 1 and 6")
+                col = input("Select column (1 - 6): ")
+    col = int(col)
     col = col - 1
-    board[0][col] = "o"
+
+    for row in range(5, -1, -1):
+        if board[row][col] == "-":
+            board[row][col] = "x"
+            return
 
 def print_board():
     for row in board:
@@ -15,3 +35,11 @@ def print_board():
             print(col, end = " ")
         print("")
 
+def main():
+    initialise_board()
+    while True:
+        userinput()
+        print_board()
+        print("")
+
+main()
